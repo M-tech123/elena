9d95df363fc83b50c3628c747f540c2b1bce1715
Muhammad Salah <ms505351@gmail.com>
2026-06-05 20:29:35 +0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use empty string or './' for relative paths, which works everywhere
-  base: '', 
+  base: '/', 
 })
